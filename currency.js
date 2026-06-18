2c099e2ced1af0c007130442653f75dfaba88df7
Romyajit-Sahoo <romyajitsahoo@gmail.com>
2026-06-18 18:26:27 +0530

    Update currency API URL to use latest USD rates

--- currency.js
+++ currency.js
@@ -4,7 +4,7 @@ let from = document.querySelector("#from");
 let to = document.querySelector("#to");
 let result = document.querySelector("#result");
 let amount = document.querySelector("#amount")
-const URL = "currency_api_key";
+const URL = "https://open.er-api.com/v6/latest/USD";
 
 
 for (let option in countries) {
